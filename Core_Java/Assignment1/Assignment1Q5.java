6ce11cdae3ca8059a34c4fedee8acaf9c8086762
Yash Raj <49111663+Ryash13@users.noreply.github.com>
2021-02-06 14:39:25 +0530

    Ques_5 Assignment_1

--- Core_Java/Assignment1/Assignment1Q5.java
+++ Core_Java/Assignment1/Assignment1Q5.java
@@ -1,45 +1,26 @@
-import java.util.*;
 import java.lang.*;
+import java.util.*;
 
 class TaxAmount{
-    public double calculateTaxAmount(int ctc)
-    {
-        double toPay=0;
-        if(ctc<=180000)
-        {
-            toPay = 0;
-            return toPay;
-        }
-        else if(ctc>=181001 & ctc>=300000)
-        {
-            toPay=ctc*0.1;
-            return toPay;
-        }
-        else if(ctc>=300001 && ctc>=500000)
-        {
-            toPay=ctc*0.2;
-            return toPay;
-        }
-        else if(ctc>= 500001 && ctc>=1000000)
-        {
-            toPay=ctc*0.3;
-            return toPay;
-        }
-        else
-        {
-            toPay=ctc*0.1;
-            return toPay;
-        }
+    public double calculateTaxAmount(int ctc){
+    	double tax = 0;
+    	if(ctc<=180000) 
+    		tax = 0;
+    	else if(ctc >=180001 && ctc <= 300000)
+    		tax = 0.1;
+    	else if(ctc>=300001 && ctc<=500000) 
+    		tax = 0.2;
+    	else if(ctc>=500001 && ctc<=1000000)
+    		tax = 0.3;
+    		
+    	double amount = ctc*tax;
+    	return amount;
     }
 }
-
 public class Assignment1Q5 {
-    public static void main(String args[]) 
-    {
-        TaxAmount obj = new TaxAmount();
-        int ctc;
-        Scanner scr = new Scanner(System.in);
-        ctc = scr.nextInt();
-        System.out.println(new TaxAmount().calculateTaxAmount(ctc));
-    }
+    public static void main(String args[]) {
+    	Scanner scr = new Scanner(System.in);
+    	int ctc = scr.nextInt();
+    	System.out.println(new TaxAmount().calculateTaxAmount(ctc));
+   }
 }
