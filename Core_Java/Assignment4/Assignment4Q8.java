75db2a2ff25043786f50b0d4459587819b14fab4
Yash Raj <49111663+Ryash13@users.noreply.github.com>
2021-02-11 16:14:27 +0530

    Ques_8 Assignment_4

---
+++ Core_Java/Assignment4/Assignment4Q8.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Assignment4Q8 implements Runnable 
+{
+    public static void printList(List<Integer> list) {
+    for(Integer i : list) {
+        System.out.println(i);
+    }
+}
+
+	public static void main(String[] args)
+	{
+      Assignment4Q8 obj = new Assignment4Q8();
+      Thread thread = new Thread(obj);
+      thread.start();
+	}
+
+	@Override
+	public void run() 
+	{
+      List<Integer> list = new ArrayList<>();
+      list.add(1);
+      list.add(2);
+      list.add(3);
+      list.add(4);
+      Consumer<List<Integer>> consumer = Assignment4Q8::printList;
+      consumer.accept(list);
+	}
+}
