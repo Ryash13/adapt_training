05c48841a340e36b9b39bb71a9e7aaa351d0944a
Yash Raj <49111663+Ryash13@users.noreply.github.com>
2021-02-27 13:47:02 +0530

    Question_2 Assignment_1

---
+++ Spring Security/Assignment 1/SecurityQuestion2/src/test/java/io/assignment/question2/SecurityQuestion2ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.assignment.question2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityQuestion2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
