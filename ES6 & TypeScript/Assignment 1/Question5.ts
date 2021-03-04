bde919f5a7d2f97ac78b4e6de94491ac66c0a725
Yash Raj <49111663+Ryash13@users.noreply.github.com>
2021-03-04 12:03:52 +0530

    Question_5 Assignment_1

---
+++ ES6 & TypeScript/Assignment 1/Question5.ts
@@ -0,0 +1,28 @@
+// 5.a
+
+function add(x=10, y=20){
+    return x+y;
+}
+
+console.log(add());
+console.log(add(45,20));
+
+// 5.b 
+
+function userFriends(username, ...friends){
+    for(let i in friends){
+        console.log(username+" "+friends[i]);
+        
+    } 
+}
+userFriends('Yash','Hemant','Suraj','Harshit','Sachin');
+
+function printCapitalNames(name1, name2, name3, name4, name5){
+    console.log(name1.toUpperCase());
+    console.log(name2.toUpperCase());
+    console.log(name3.toUpperCase());
+    console.log(name4.toUpperCase());
+    console.log(name5.toUpperCase());
+} 
+let arr = ['john','karan','shravan','hritik','rohan'];
+printCapitalNames(...arr);
