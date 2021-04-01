a6c11cee1e45efecb9b3cecc786207ca18fe8ca4
Yash Raj <49111663+Ryash13@users.noreply.github.com>
2021-04-01 11:02:00 +0530

    Delete Question9 directory

--- Question9/src/main/java/io/assignment/Repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package io.assignment.Repository;
-
-import io.assignment.Model.Customer;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CustomerRepository extends CrudRepository<Customer , Integer> {
-}
